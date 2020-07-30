554ad8c62dcb81501935dc1bd3f18ba071e26062
Hitanshu Panchal <hyp9820@gmail.com>
2020-07-30 16:39:12 +0530

    NCR and Unusual Behaviour

--- home/views.py
+++ home/views.py
@@ -102,19 +102,19 @@ def home(request, user):
     list3 = []
     for doc in docs:
         email = db.collection(u'users').document(u'{}'.format(doc.id)).get().to_dict()['email']
-        penddingSubdocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'all_data').where(u'Status',u'==',u'Pending').get()
+        penddingSubdocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'FIR').where(u'Status',u'==',u'Pending').get()
         for subdoc in penddingSubdocs:
             list1.append((subdoc.id,doc.id,email))
 
-        inProgressDocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'all_data').where(u'Status',u'==',u'inProgress').get()
+        inProgressDocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'FIR').where(u'Status',u'==',u'inProgress').get()
         for subdoc in inProgressDocs:
             list2.append((subdoc.id,doc.id,email))
 
-        assessedDocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'all_data').where(u'Status',u'==',u'Assessed').get()
+        assessedDocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'FIR').where(u'Status',u'==',u'Assessed').get()
         for subdoc in assessedDocs:
             list3.append((subdoc.id,doc.id,email))
 
-    return render(request, "home.html", {"user":request.session['username'], "category":"FIR_NCR", "pending_id_list":list1, "progress_id_list":list2, "assessed_id_list":list3, "fir_active": "active", "assesedNo" : len(list3), "progressNo":len(list2), "newNo":len(list1) })
+    return render(request, "home.html", {"user":request.session['username'], "category":"FIR", "pending_id_list":list1, "progress_id_list":list2, "assessed_id_list":list3, "fir_active": "active", "assesedNo" : len(list3), "progressNo":len(list2), "newNo":len(list1) })
 
 def details(request, category, user, uid, case_id):
 
@@ -125,6 +125,7 @@ def details(request, category, user, uid, case_id):
 
     metadata = {'category':category, 'user':user, 'uid':uid, 'case_id':case_id}
 
+    # Bride Report
     if category == "PaidBribe":
         if request.GET.get('delete')=='del':
             db.collection(category).document(uid).collection(u'all_data').document(case_id).delete()
@@ -152,10 +153,20 @@ def details(request, category, user, uid, case_id):
             }, merge=True)
         return render(request, "bribeDetails.html", {"user":request.session['username'], "data":data, "metadata":metadata})
 
+    # FIR
+    if category == "FIR":
+        category = "FIR_NCR"
+        if request.GET.get('delete')=='del':
+            db.collection(category).document(uid).collection(u'FIR').document(case_id).delete()
+            return redirect(home, user=user)
+        if request.GET.get('accept')=='accpt':
+            db.collection(category).document(uid).collection(u'FIR').document(case_id).set({
+                u'Status': u'Assessed'
+            }, merge=True)
+            return redirect(home, user=user)
 
-    if category == "FIR_NCR":
         print("***************",category,"**************")
-        doc_ref = db.collection(category).document(uid).collection(u'all_data').document(case_id)
+        doc_ref = db.collection(category).document(uid).collection(u'FIR').document(case_id)
         doc = doc_ref.get()
         case_data = doc.to_dict()
         data = {}
@@ -163,12 +174,47 @@ def details(request, category, user, uid, case_id):
             data[i] = case_data[i]
         # print(data)
         if data['Status'] == 'Pending':
-            db.collection(category).document(uid).collection(u'all_data').document(case_id).set({
+            db.collection(category).document(uid).collection(u'FIR').document(case_id).set({
                 u'Status': u'inProgress'
             }, merge=True)
         return render(request, "details.html", {"user":request.session['username'], "data":data, "metadata":metadata})
 
+    # NCR
+    if category == "NCR":
+        category = "FIR_NCR"
+        if request.GET.get('delete')=='del':
+            db.collection(category).document(uid).collection(u'NCR').document(case_id).delete()
+            return redirect(home, user=user)
+        if request.GET.get('accept')=='accpt':
+            db.collection(category).document(uid).collection(u'NCR').document(case_id).set({
+                u'Status': u'Assessed'
+            }, merge=True)
+            return redirect(home, user=user)
+
+        print("***************",category,"**************")
+        doc_ref = db.collection(category).document(uid).collection(u'NCR').document(case_id)
+        doc = doc_ref.get()
+        case_data = doc.to_dict()
+        data = {}
+        for i in sorted(case_data):
+            data[i] = case_data[i]
+        # print(data)
+        if data['Status'] == 'Pending':
+            db.collection(category).document(uid).collection(u'NCR').document(case_id).set({
+                u'Status': u'inProgress'
+            }, merge=True)
+        return render(request, "details.html", {"user":request.session['username'], "data":data, "metadata":metadata})
+
+    # NOC
     if category == "NOC":
+        if request.GET.get('delete')=='del':
+            db.collection(category).document(uid).collection(u'all_data').document(case_id).delete()
+            return redirect(noc, user=user)
+        if request.GET.get('accept')=='accpt':
+            db.collection(category).document(uid).collection(u'all_data').document(case_id).set({
+                u'Status': u'Accepted'
+            }, merge=True)
+            return redirect(noc, user=user)
         print("***************",category,"**************")
         doc_ref = db.collection(category).document(uid).collection(u'all_data').document(case_id)
         doc = doc_ref.get()
@@ -183,6 +229,30 @@ def details(request, category, user, uid, case_id):
             }, merge=True)
         return render(request, "nocDetails.html", {"user":request.session['username'], "data":data, "metadata":metadata})
 
+    # Unusual Behaviour
+    if category == "UnusualBehaviour":
+        if request.GET.get('delete')=='del':
+            db.collection(category).document(uid).collection(u'all_data').document(case_id).delete()
+            return redirect(unusualBehaviour, user=user)
+        if request.GET.get('accept')=='accpt':
+            db.collection(category).document(uid).collection(u'all_data').document(case_id).set({
+                u'Status': u'Accepted'
+            }, merge=True)
+            return redirect(unusualBehaviour, user=user)
+
+        print("***************",category,"**************")
+        doc_ref = db.collection(category).document(uid).collection(u'all_data').document(case_id)
+        doc = doc_ref.get()
+        case_data = doc.to_dict()
+        data = {}
+        for i in sorted(case_data):
+            data[i] = case_data[i]
+        # print(data)
+        if data['Status'] == 'Pending':
+            db.collection(category).document(uid).collection(u'all_data').document(case_id).set({
+                u'Status': u'In Process'
+            }, merge=True)
+        return render(request, "ubDetails.html", {"user":request.session['username'], "data":data, "metadata":metadata})
     
 def paidBribe(request, user):
 
@@ -230,6 +300,7 @@ def hotReport(request, user):
     for doc in docs:
 
         email = db.collection(u'users').document(u'{}'.format(doc.id)).get().to_dict()['email']
+        # print(email)
         reports = db.collection(u'Hot Report').document(u'{}'.format(doc.id)).collection(u'all_data').get()
         for report in reports:
             lst.append((report.id,doc.id, email, report.to_dict()))
@@ -238,7 +309,37 @@ def hotReport(request, user):
     return render(request, "hotReport.html",  {"user":request.session['username'], "category":"hot", "hot_active": "active", "data":lst})
     
 def unusualBehaviour(request, user):
-    return render(request, "unusualBehaviour.html", {"user":request.session['username'], "bribe_active": "active"})
+    if not request.session.has_key('username'):
+        return redirect(login)
+    if request.session['username'] != user:
+        return redirect(login)
+
+    docs = db.collection(u'UnusualBehaviour').stream()
+    list1 = []
+    list2 = []
+    list3 = []
+    total_cases = 0
+    pending_cases = 0
+    for doc in docs:
+        email = db.collection(u'users').document(u'{}'.format(doc.id)).get().to_dict()['email']
+        pendingSubdocs = db.collection(u'UnusualBehaviour').document(u'{}'.format(doc.id)).collection(u'all_data').where(u'Status',u'==',u'Pending').get()
+        for subdoc in pendingSubdocs:
+            list1.append((subdoc.id,doc.id, email))
+
+        inProcessDocs = db.collection(u'UnusualBehaviour').document(u'{}'.format(doc.id)).collection(u'all_data').where(u'Status',u'==',u'In Process').get()
+        for subdoc in inProcessDocs:
+            list2.append((subdoc.id,doc.id, email))
+
+        acceptedDocs = db.collection(u'UnusualBehaviour').document(u'{}'.format(doc.id)).collection(u'all_data').where(u'Status',u'==',u'Accepted').get()
+        for subdoc in acceptedDocs:
+            list3.append((subdoc.id,doc.id, email))
+
+    pending_cases = len(list1)
+    inprocess_cases = len(list2)
+    accepted_cases = len(list3)
+    total_cases = (len(list1)+len(list2)+len(list3))
+
+    return render(request, "unusualBehaviour.html", {"user":request.session['username'], "pending_cases":pending_cases, "inprocess_cases":inprocess_cases, "accepted_cases":accepted_cases, "total_cases":total_cases, "category":"UnusualBehaviour", "bribe_active": "active", "pending":list1, "inprocess":list2, "accepted":list3})
 
 def noc(request, user):
 
@@ -269,7 +370,31 @@ def noc(request, user):
     return render(request, "noc.html", {"user":request.session['username'], "noc_active": "active", "category":"NOC", "pending":list1, "inprocess":list2, "accepted":list3})
 
 def ncr(request, user):
-    return render(request, "ncr.html", {"user":request.session['username'], "ncr_active": "active"})
+
+    if not request.session.has_key('username'):
+        return redirect(login)
+    if request.session['username'] != user:
+        return redirect(login)
+    
+    docs = db.collection(u'FIR_NCR').stream()
+    list1 = []
+    list2 = []
+    list3 = []
+    for doc in docs:
+        email = db.collection(u'users').document(u'{}'.format(doc.id)).get().to_dict()['email']
+        penddingSubdocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'NCR').where(u'Status',u'==',u'Pending').get()
+        for subdoc in penddingSubdocs:
+            list1.append((subdoc.id,doc.id,email))
+
+        inProgressDocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'NCR').where(u'Status',u'==',u'inProgress').get()
+        for subdoc in inProgressDocs:
+            list2.append((subdoc.id,doc.id,email))
+
+        assessedDocs = db.collection(u'FIR_NCR').document(u'{}'.format(doc.id)).collection(u'NCR').where(u'Status',u'==',u'Assessed').get()
+        for subdoc in assessedDocs:
+            list3.append((subdoc.id,doc.id,email))
+
+    return render(request, "ncr.html", {"user":request.session['username'], "category":"NCR", "pending_id_list":list1, "progress_id_list":list2, "assessed_id_list":list3, "ncr_active": "active", "assesedNo" : len(list3), "progressNo":len(list2), "newNo":len(list1) })
 
 def police(request):
     return render(request, "police.html")
